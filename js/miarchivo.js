5ad30888a8d3778e69857f3e590e7149bc9acc57
jonathanpoblet <jonathanpoblet228@gmail.com>
2022-08-05 16:29:44 -0300

    agregando más js

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -41,3 +41,43 @@ for(i=0; i<6 ; i++){
 }
 */
 
+class Producto {
+    constructor(nombre,categoria,precioCosto){
+        this.nombre = nombre.toUpperCase();
+        this.categoria = categoria.toUpperCase();
+        this.precioCosto = parseFloat(precioCosto);
+    }
+    sumaIva() {
+        this.precioConImpuestos = this.precioCosto * 1.21;
+    }
+}
+
+const productos = [];
+
+productos.push(new Producto("selva negra","tortas",3100));
+productos.push(new Producto("chocotorta","tortas",2700));
+productos.push(new Producto("torta frutilla con crema","tortas",2400));
+productos.push(new Producto("torta de chocolate","tortas",2400));
+productos.push(new Producto("torta de zanahoria","tortas",2100));
+productos.push(new Producto("bizcochuelo de limon","tortas",1850));
+productos.push(new Producto("torta cumpleaños hombre","tortas",4100));
+productos.push(new Producto("torta cumpleaños mujer","tortas",4100));
+productos.push(new Producto("macarons 3 unidades","macarons",300));
+productos.push(new Producto("macarons 6 unidades","macarons",500));
+productos.push(new Producto("macarons 24 unidades","macarons", 2050));
+productos.push(new Producto("cupcakes chocolate x 6","cupcakes",850));
+productos.push(new Producto("cupcakes vainilla x 6","cupcakes",850));
+productos.push(new Producto("cupcakes personalizados x 6","cupcakes",850));
+productos.push(new Producto("box dulce dia del padre","box dulce",2000));
+productos.push(new Producto("box dulce dia de la madre","box dulce", 2000));
+productos.push(new Producto("box dulce variado","box dulce", 2000));
+productos.push(new Producto("box dulce frutilla","box dulce", 2000));
+productos.push(new Producto("box dulce cumpleaños","box dulce", 2000));
+
+
+console.log("En total, Pasteleria Riccione tiene " + productos.length + " productos:");
+
+for(const producto of productos){
+    producto.sumaIva();
+}
+console.log(productos);
