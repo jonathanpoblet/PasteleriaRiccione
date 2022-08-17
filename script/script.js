861d4e40fc1baa1968b5c8825f1793c9fdb8712a
jonathanpoblet <jonathanpoblet228@gmail.com>
2022-08-17 15:44:43 -0300

    Agregando eventos

--- script/script.js
+++ script/script.js
@@ -1,35 +1,45 @@
-//creador de productos
+//Creador de productos
 class Producto {
-    constructor(nombre,categoria,precioCosto,foto){
+    constructor(nombre,categoria,precioCosto,foto,identificador){
         this.nombre = nombre.toUpperCase();
         this.categoria = categoria.toUpperCase();
         this.precioCosto = parseFloat(precioCosto);
         this.foto = foto
+        this.identificador = identificador
+    }
+}
+
+//Creardor de los elementos que vayamos pusheando al carrito
+class ElementoCarrito{
+    constructor(producto,cantidad){
+        this.producto = producto;
+        this.cantidad = cantidad;
     }
 }
 
 const productos = [];
+const carrito = [];
 
 //productos ingresados al array
-productos.push(new Producto("torta selva negra","tortas",3100,`../fotos/selva-negra.jpg`));
-productos.push(new Producto("chocotorta","tortas",2700,`../fotos/chocotorta.jpg`));
-productos.push(new Producto("box dulce padre","box dulce",2000,`../fotos/boxdulce-padre.jpg`));
-productos.push(new Producto("box dulce madre","box dulce", 2000,`../fotos/boxdulce-madre.jpg`));
-productos.push(new Producto("torta de zanahoria","tortas",2100,`../fotos/torta-zanahoria.jpg`));
-productos.push(new Producto("box dulce frutilla","box dulce", 2000,`../fotos/boxdulce-frutilla.jpg`));
-productos.push(new Producto("torta cumpleaños hombre","tortas",4100,`../fotos/torta-cumpleaños.jpg`));
-productos.push(new Producto("torta cumpleaños mujer","tortas",4100,`../fotos/torta-cumpleaños-mujer.jpg`));
-productos.push(new Producto("macarons 3 unidades","macarons",300,`../fotos/macaron-3.jpg`));
-productos.push(new Producto("torta de limon","tortas",1850,`../fotos/bizcochuelo-limon.jpg`));
-productos.push(new Producto("macarons 6 unidades","macarons",500,`../fotos/macarons-6.jpg`));
-productos.push(new Producto("cupcakes chocolate x 6","cupcakes",850,`../fotos/cupcakes-6unidades.jpg`));
-productos.push(new Producto("cupcakes vainilla x 6","cupcakes",850,`../fotos/cupcakes-6unidades-vainilla.jpg`));
-productos.push(new Producto("cupcakes mix x 6","cupcakes",850,`../fotos/cupcakes-personalizados.jpg`));
-productos.push(new Producto("box dulce variado","box dulce", 2000,`../fotos/boxdulce-variado.jpeg`));
-productos.push(new Producto("torta frutilla con crema","tortas",2400,`../fotos/torta-frutilla.jpg`));
-productos.push(new Producto("torta de chocolate","tortas",2400,`../fotos/torta-de-chocolate.jpg`));
-productos.push(new Producto("box dulce cumpleaños","box dulce", 2000,`../fotos/boxdulcecumpleaños.jpg`));
-productos.push(new Producto("macarons 24 unidades","macarons", 2050,`../fotos/macaron-24.jpg`));
+productos.push(new Producto("torta selva negra","tortas",3100,`../fotos/selva-negra.jpg`,1));
+productos.push(new Producto("chocotorta","tortas",2700,`../fotos/chocotorta.jpg`,2));
+productos.push(new Producto("box dulce padre","box dulce",2000,`../fotos/boxdulce-padre.jpg`,3));
+productos.push(new Producto("box dulce madre","box dulce", 2000,`../fotos/boxdulce-madre.jpg`,4));
+productos.push(new Producto("torta de zanahoria","tortas",2100,`../fotos/torta-zanahoria.jpg`,5));
+productos.push(new Producto("box dulce frutilla","box dulce", 2000,`../fotos/boxdulce-frutilla.jpg`,6));
+productos.push(new Producto("torta cumpleaños hombre","tortas",4100,`../fotos/torta-cumpleaños.jpg`,7));
+productos.push(new Producto("torta cumpleaños mujer","tortas",4100,`../fotos/torta-cumpleaños-mujer.jpg`,8));
+productos.push(new Producto("macarons 3 unidades","macarons",300,`../fotos/macaron-3.jpg`,9));
+productos.push(new Producto("torta de limon","tortas",1850,`../fotos/bizcochuelo-limon.jpg`,10));
+productos.push(new Producto("macarons 6 unidades","macarons",500,`../fotos/macarons-6.jpg`,11));
+productos.push(new Producto("cupcakes chocolate x 6","cupcakes",850,`../fotos/cupcakes-6unidades.jpg`,12));
+productos.push(new Producto("cupcakes vainilla x 6","cupcakes",850,`../fotos/cupcakes-6unidades-vainilla.jpg`,13));
+productos.push(new Producto("cupcakes mix x 6","cupcakes",850,`../fotos/cupcakes-personalizados.jpg`,14));
+productos.push(new Producto("box dulce variado","box dulce", 2000,`../fotos/boxdulce-variado.jpeg`,15));
+productos.push(new Producto("torta frutilla con crema","tortas",2400,`../fotos/torta-frutilla.jpg`,16));
+productos.push(new Producto("torta de chocolate","tortas",2400,`../fotos/torta-de-chocolate.jpg`,17));
+productos.push(new Producto("box dulce cumpleaños","box dulce", 2000,`../fotos/boxdulcecumpleaños.jpg`,18));
+productos.push(new Producto("macarons 24 unidades","macarons", 2050,`../fotos/macaron-24.jpg`,19));
 
 console.log("En total, Pasteleria Riccione tiene " + productos.length + " productos:");
 
@@ -46,7 +56,8 @@ const productosConImpuestos = productos.map(productos => {
         nombre: productos.nombre,
         categoria: productos.categoria,
         precioConImpuestos: parseFloat(productos.precioCosto * 1.21),
-        foto: productos.foto
+        foto: productos.foto,
+        identificador: productos.identificador
     }
 })
 console.log(productosConImpuestos)
@@ -93,6 +104,23 @@ function crearCartas(producto){
     contenedorCarta.className = "article-productos-container"
     contenedorCarta.append(carta);
 
+
+    //Evento para boton
+    botonCarta.onclick = () => {
+        let elementoExistente=carrito.find((elemento) => elemento.producto.identificador == producto.identificador)
+        if(elementoExistente){
+            elementoExistente.cantidad+=1;
+            alert(`INGRESASTE ${producto.nombre} AL CARRITO`)
+        }else{
+            let nuevoElementoCarrito = new ElementoCarrito(producto,1);
+            carrito.push(nuevoElementoCarrito);
+            alert(`INGRESASTE ${producto.nombre} AL CARRITO`)
+        }
+        dibujarCarrito();
+    }
+
+
+
     return contenedorCarta;
 }
 
@@ -106,6 +134,82 @@ function hacerCards(){
     );
 }
 hacerCards();
+
+//----------------------------------------------------------------------
+
+const contenedorCarrito = document.getElementById("contenedorCarrito");
+const contenedorFooterCarrito = document.getElementById("footer")
+
+function dibujarCarrito(){
+    contenedorCarrito.innerHTML = "";
+
+    let suma = 0;
+
+    carrito.forEach(
+        (elemento) => {
+            let renglonCarrito = document.createElement("tr")
+
+            renglonCarrito.innerHTML= `
+                <tr>
+                    <td>${elemento.producto.identificador}</td>
+                    <td>${elemento.producto.nombre}</td>
+                    <td><input id="cantidad-producto-${elemento.producto.identificador}" type="number" value="${elemento.cantidad}" min="1" max="500" step="1"</td>
+                    <td>$${elemento.producto.precioConImpuestos}</td>
+                    <td>$${elemento.producto.precioConImpuestos * elemento.cantidad}</td>
+                    <td><button id="eliminar-producto-${elemento.producto.identificador}" type="button" class="btn btn-danger"><i class="bi bi-trash"></i></button></td>
+                </tr>
+            `;
+
+            contenedorCarrito.append(renglonCarrito);
+
+            suma+= elemento.producto.precioConImpuestos * elemento.cantidad
+
+            let inputCantidadProductos = document.getElementById(`cantidad-producto-${elemento.producto.identificador}`)
+        
+            inputCantidadProductos.addEventListener("change", (e) => {
+               let nuevaCantidad = e.target.value;
+               elemento.cantidad = nuevaCantidad;
+               dibujarCarrito();
+            } );
+
+            let botonBorrar = document.getElementById(`eliminar-producto-${elemento.producto.identificador}`)
+                botonBorrar.addEventListener("click", () => {
+                    borrarProductoCarrito(elemento);
+                    dibujarCarrito();
+                });
+        }
+    );
+
+    if(carrito.length == 0){
+        contenedorFooterCarrito.innerHTML = `
+        <th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>
+        `;
+    }else{
+        contenedorFooterCarrito.innerHTML = `
+        <th scope="row" colspan="5">Total: $${suma}</th>
+        `;
+    }
+}
+
+function borrarProductoCarrito(elementoEliminado){
+    const elementosNoBorrados = carrito.filter(
+        (elemento) => elemento.producto.identificador != elementoEliminado.producto.identificador
+    );
+
+    carrito.length = 0;
+
+    elementosNoBorrados.forEach((elemento) => carrito.push(elemento))
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
